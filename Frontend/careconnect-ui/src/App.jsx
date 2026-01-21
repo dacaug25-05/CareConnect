e246c9077e38b919476087b4d016e063ebc7fd27
Anurag <anuragteurwade9612@gamil.com>
2026-01-22 02:56:14 +0530

    Initial commit

---
+++ Frontend/careconnect-ui/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import logo from './assets/logo.png'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+
+      <div>
+        <img src={logo} alt="CareConnect Logo"  width={150} height={100}/>
+   <h4>Welcome to CareConnect</h4>
+    </div>
+  )
+}
+
+export default App
